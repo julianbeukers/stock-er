a9890ad7a118c6d443605712fed1090303f6f8fc
Kausthub Jadhav <kjadhav@uci.edu>
2017-03-17 21:51:23 -0700

    Add steps to approach problem

--- stock-er.py
+++ stock-er.py
@@ -1,1 +1,9 @@
+# In this program, I attempt to build three different models that
+# predict the prices of Apple Stock
+# then plot them all on a graph to compare the results
 
+# The steps would be:
+# 1. Install Dependencies
+# 2. Collect Dataset
+# 3. Write Script
+# 4. Analyze Graph
